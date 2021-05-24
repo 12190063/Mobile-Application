4a49a3699bb7819cd10589faf80d22f2bf8a1bdc
Kuenga420 <tsheringkuengatenzin@gmail.com>
2021-05-24 16:23:31 +0600

    TODO_20_ii

--- TODO_20_ii/app/src/androidTest/java/com/gcit/todo_20_ii/SpinnerSelectionTest.java
+++ TODO_20_ii/app/src/androidTest/java/com/gcit/todo_20_ii/SpinnerSelectionTest.java
@@ -1,25 +1,57 @@
 package com.gcit.todo_20_ii;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 
 /**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * PhoneNumberSpinner is an app that shows a spinner, with the id label_spinner,
+ * for choosing the label of a phone number (Home, Work, Mobile, and Other).
+ * The app displays the choice in a text field, concatenated with the entered phone number.
+ * The goal of this test is to open the spinner, click each item,
+ * and then verify that the TextView text_phonelabel contains the item.
+ * The test demonstrates that the code retrieving the spinner selection
+ * is working properly, and the code displaying the text of the spinner item
+ * is also working properly.
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class SpinnerSelectionTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.gcit.todo_20_ii", appContext.getPackageName());
+    public void iterateSpinnerItems() {
+        String[] myArray = mActivityRule.getActivity().getResources()
+                .getStringArray(R.array.labels_array);
+
+        // Iterate through the spinner array of items.
+        int size = myArray.length;
+        for (int i=0; i<size; i++) {
+            // Find the spinner and click on it.
+            onView(withId(R.id.label_spinner)).perform(click());
+            // Find the spinner item and click on it.
+            onData(is(myArray[i])).perform(click());
+            // Find the Submit button and click on it.
+            onView(withId(R.id.button_main)).perform(click());
+            // Find the text view and check that the spinner item
+            // is part of the string.
+            onView(withId(R.id.text_phonelabel))
+                    .check(matches(withText(containsString(myArray[i]))));
+        }
     }
 }
