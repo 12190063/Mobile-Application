2efe5b5dff76a1932d71ac6b7379511c00e81ee6
Kuenga420 <tsheringkuengatenzin@gmail.com>
2021-05-27 12:45:03 +0600

    TODO_22

---
+++ TODO_22_ii/app/src/main/java/com/gcit/todo_22_ii/BookLoader.java
@@ -0,0 +1,9 @@
+package com.gcit.todo_22_ii;
+
+import android.support.v4.content.Loader;
+
+public class BookLoader extends Loader<String> {
+    public BookLoader(MainActivity mainActivity, String queryString) {
+        super();
+    }
+}
---
+++ TODO_22_ii/app/src/main/java/com/gcit/todo_22_ii/NetworkUtils.java
@@ -0,0 +1,4 @@
+package com.gcit.todo_22_ii;
+
+public class NetworkUtils {
+}
